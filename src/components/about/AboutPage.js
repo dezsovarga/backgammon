3f50bcab8fd4555df68671d318ea448c5dbe86c3
Varga Dezso <dezso.varga>
2016-07-12 00:13:26 +0300

    added react router

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+	render () {
+		return (
+			<div>
+				<h1> About </h1>
+				<p> backgammon app uses React, redux and react router </p>
+			</div>
+		);
+	}
+}
+
+export default AboutPage;
