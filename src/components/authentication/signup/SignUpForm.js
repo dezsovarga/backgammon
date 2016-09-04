8af1b440873bf4c20e998eb656f7fcbe2e54552e
Varga Dezso <dezso.varga>
2016-09-05 00:40:41 +0300

    started to add validation to signup form

--- src/components/authentication/signup/SignUpForm.js
+++ src/components/authentication/signup/SignUpForm.js
@@ -1,26 +1,111 @@
 import React, { PropTypes } from 'react';
 import {reduxForm} from 'redux-form';
+import {Grid, Row, Col, PageHeader, Panel} from 'react-bootstrap';
+import ValidationField from '../../common/formFields/ValidationField.jsx';
 
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less'
+    }
+    if (!values.lastName) {
+        errors.lastName = 'Required'
+    } else if (values.lastName.length > 15) {
+        errors.lastName = 'Must be 15 characters or less'
+    }
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
 
+    return errors
+}
 class SignUpForm extends React.Component {
+
     render () {
-        const {fields: {firstName, lastName, email}, handleSubmit} = this.props;
-        return (
-            <form onSubmit={handleSubmit(this.props.onRegisterSubmit)}>
-                <div>
-                    <label>First Name</label>
-                    <input type="text" placeholder="First Name" {...firstName}/>
+        const {fields: {firstName, lastName, email, password, confirmPassword}, handleSubmit} = this.props;
+        let content = (
+            <form className="form-horizontal"
+                  onSubmit={ handleSubmit(this.props.onRegisterSubmit) }>
+                <div className="form-group">
+                    <label className="control-label col-xs-3">First Name</label>
+                    <div className="col-xs-9">
+                        <ValidationField
+                            type="text"
+                            placeholder="First Name"
+                            fieldType="input"
+                            {...firstName}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Last Name</label>
-                    <input type="text" placeholder="Last Name" {...lastName}/>
+                <div className="form-group">
+                    <label className="control-label col-xs-3">Last Name</label>
+                    <div className="col-xs-9">
+                        <ValidationField
+                            type="text"
+                            placeholder="Last Name"
+                            fieldType="input"
+                            {...lastName}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" placeholder="Email" {...email}/>
+                <div className="form-group">
+                    <label className="control-label col-xs-3">Email</label>
+                    <div className="col-xs-9">
+                        <input type="email"
+                               className="form-control"
+                               id="inputEmail"
+                               placeholder="Email"
+                               {...email}
+                        />
+                    </div>
                 </div>
-                <button type="submit">Submit</button>
+                <div className="form-group">
+                    <label className="control-label col-xs-3">Password</label>
+                    <div className="col-xs-9">
+                        <input type="password"
+                               className="form-control"
+                               id="inputPassword"
+                               placeholder="Password"
+                               {...password}
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="control-label col-xs-3">Confirm Password</label>
+                    <div className="col-xs-9">
+                        <input type="password"
+                               className="form-control"
+                               id="inputConfirmPassword"
+                               placeholder="Confirm Password"
+                               {...confirmPassword}
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-xs-offset-2 col-xs-10">
+                        <div className="checkbox">
+                            <label><input type="checkbox"/> Remember me</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-xs-offset-2 col-xs-10">
+                        <button type="submit" className="btn btn-primary">Register</button>
+                    </div>
+                </div>
             </form>
+
+        );
+        return (
+            <div className="logo-panel-wrapper">
+                <Panel header="Sign up" className="logo-panel">
+                    {content}
+                </Panel>
+            </div>
         );
     }
 }
@@ -38,5 +123,6 @@ SignUpForm.propTypes = {
 
 export default reduxForm({ // <----- THIS IS THE IMPORTANT PART!
     form: 'SignUpForm',                           // a unique name for this form
-    fields: ['firstName', 'lastName', 'email'] // all the fields in your form
+    fields: ['firstName', 'lastName', 'email', 'password', 'conformPassword'], // all the fields in your form
+    validate
 })(SignUpForm);
---
+++ src/components/common/formFields/ValidationField.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import classNames from 'classnames';
+import * as CONSTANTS from './constants';
+
+export default class ValidationField extends React.Component {
+
+    getFeedback() {
+        const { feedback } = this.props;
+        return (
+            <label
+                className="feedback clickable-text"
+                onClick={ () => {
+                    return this.props.feedbackHandler ? this.props.feedbackHandler() : null;
+                } }
+            >
+                { feedback }
+            </label>
+        );
+    }
+
+    render() {
+        let content = null;
+        const { error, touched, feedback } = this.props;
+        const inputClass = classNames(
+            this.props.className,
+            {
+                'validate-form-control': true,
+                'has-error': error && touched,
+                'has-feedback': !!feedback
+            }
+        );
+        let control;
+        switch (this.props.fieldType) {
+            case CONSTANTS.INPUT:
+                control = <input className="form-control" {...this.props} />;
+                break;
+            case CONSTANTS.TEXTAREA:
+                control = <textarea {...this.props} />;
+                break;
+
+            default:
+                control = <input {...this.props} />;
+                break;
+        }
+
+        content = (
+            <div className={ inputClass }>
+                { control }
+                { touched && error && <label className="error truncate">{ error }</label> }
+                { feedback && this.getFeedback() }
+            </div>
+        );
+        return content;
+    }
+}
+
+export default ValidationField;
